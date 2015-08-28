108479cad8fce824647f98b6e8b025374a4d615d
Rui Batista <rui.batista@enear.co>
2015-08-28 18:48:12 +0100

    Removed distance stuff, step to a cannonical import format.

--- xlsx/importer.py
+++ xlsx/importer.py
@@ -10,10 +10,32 @@ import json
 import distance
 
 
+header_keys = {
+	u'autor' : u'author',
+	u'classificação' : u'category',
+	u'título' : 'title',
+	u'observações' : u'notes',
+	u'estado da obra' : u'status',
+	u'idioma' : u'language',
+	u'formato' : u'format'
+}
+
+def lower(x): return x.lower()
+
+key_processors = {x : lower for x in [u'status', u'format', u'language', u'category']}
+
+def process_dict(d):
+	# bad code
+	r = {}
+	for k, v in d.iteritems():
+		f = key_processors.get(k, lambda x : x)
+		r[k] = f(v)
+	return r
+
 def sheet2dict(sheet):
     headers = [cell.value for cell in sheet.rows[0]]
-    d = [{k.lower() : v.value for (k, v) in zip(headers, row) if k is not None and v.value is not None} for row in sheet.rows[1:]]
-    return d
+    d = [{header_keys[k.lower()] : v.value for (k, v) in zip(headers, row) if k is not None and v.value is not None} for row in sheet.rows[1:]]
+    return [process_dict(i) for i in d]
 
 
 def by_key(d, key):
@@ -30,27 +52,18 @@ def print_categories(d):
 
 distance_treshold = 1
 
+def similar(l):
+	return ((c1, c2) for (c1, c2) in itertools.combinations(l, 2) if distance.levenshtein(c1, c2) <= distance_treshold)
+
 def problematic_categories(d):
-	cs = by_key(d,u'classificação')
-	for c1, c2 in itertools.combinations(cs, 2):
-		if distance.levenshtein(c1, c2) <= distance_treshold:
-			print "%s --> %s" % (c1, c2)
+	for c1, c2 in similar(by_key(d, u'classificação')):
+		print "%s --> %s" % (c1, c2)
 
 def main(args):
-    command = args[0]
-    filename = args[1]
+    filename = args[0]
     wb = load_workbook(filename=filename)
     d = sheet2dict(wb.active)
-    if command == "json":
-        json.dump(d, sys.stdout, sort_keys=True, indent=2, ensure_ascii=False)
-    elif command =="authors":
-    	print_authors(d)
-    elif command == "categories":
-    	print_categories(d)
-    	problematic_categories(d)
-
-
-
+    json.dump(d, sys.stdout, sort_keys=True, indent=2, ensure_ascii=True)
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
