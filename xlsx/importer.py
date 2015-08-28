c1752d13ba96cc167eeee19e7cb5a083488182dc
Rui Batista <rui.batista@enear.co>
2015-08-28 15:18:57 +0100

    Add helpers to excel importer.

--- xlsx/importer.py
+++ xlsx/importer.py
@@ -1,22 +1,54 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
+import itertools
 import os
 import sys
 
 from openpyxl import *
 import json
+import distance
 
 
 def sheet2dict(sheet):
     headers = [cell.value for cell in sheet.rows[0]]
-    d = [{k : v.value for (k, v) in zip(headers, row) if v.value is not None} for row in sheet.rows[1:]]
+    d = [{k.lower() : v.value for (k, v) in zip(headers, row) if k is not None and v.value is not None} for row in sheet.rows[1:]]
     return d
 
+
+def by_key(d, key):
+    l = {i[key] for i in d}
+    return sorted(l)
+
+def print_authors(d):
+    for author in by_key(d, 'autor'):
+        print(author)
+    
+def print_categories(d):
+    for c in by_key(d, u'classificação'):
+       print(c)
+
+distance_treshold = 1
+
+def problematic_categories(d):
+	cs = by_key(d,u'classificação')
+	for c1, c2 in itertools.combinations(cs, 2):
+		if distance.levenshtein(c1, c2) <= distance_treshold:
+			print "%s --> %s" % (c1, c2)
+
 def main(args):
-    filename = args[0]
+    command = args[0]
+    filename = args[1]
     wb = load_workbook(filename=filename)
     d = sheet2dict(wb.active)
-    json.dump(d, sys.stdout, sort_keys=True, indent=2, ensure_ascii=False)
+    if command == "json":
+        json.dump(d, sys.stdout, sort_keys=True, indent=2, ensure_ascii=False)
+    elif command =="authors":
+    	print_authors(d)
+    elif command == "categories":
+    	print_categories(d)
+    	problematic_categories(d)
+
 
 
 
